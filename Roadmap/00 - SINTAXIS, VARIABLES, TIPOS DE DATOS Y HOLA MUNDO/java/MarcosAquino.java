b650a3ed06deaf689e214cdbfd280390ced3bcb4
MborisPY <155504708+MborisPY@users.noreply.github.com>
2024-01-03 00:43:47 -0300

    MarcosAquino.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/MarcosAquino.java
@@ -0,0 +1,27 @@
+public class MiPrimerReto {
+    //Esto es un comentario de una sola linea :)
+    //https://www.java.com/es/ sitio oficial
+
+    /*
+    un comentario de varias 
+    líneas.
+     */
+    /**
+     * comentario que utiliza la herramienta javadoc
+     */
+    public static void main(String args[]) {
+
+        String nombre = "Marcos";
+        byte edad = 28;
+        short a = 01234;
+        int b = 0000001;
+        long numeroLargo = 123456789L;
+        float flotante = 1.2F;
+        double pi = 3.14;
+        char letra = 'M';
+        boolean tardeBastante = true;
+
+        System.out.println("¡Hola, Java!");
+
+    }
+}
